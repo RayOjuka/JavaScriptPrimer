ec8797e6742b346e3072983f9e543d29ce09fb5c
rayweathers88 <rayweathers88@gmail.com>
2020-04-30 17:57:11 +0300

    Objects and classes in Js, TypeScript features

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+/* import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -9,4 +9,21 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  .catch(err => console.error(err)); */
+
+
+ import * as NameAndWeatherLocation from './modules/nameandweather';
+ import { Name as OtherName } from './modules/duplicatename';
+ import { TempConverter } from './tempConverter';
+
+ let cities: {[index: string]: [string, string]} = {};
+
+ cities['London'] = ['raining', TempConverter.convertFtoC('38')];
+ cities['Paris'] = ['sunny', TempConverter.convertFtoC('52')];
+ cities['Berlin'] = ['snowing', TempConverter.convertFtoC('52')];
+
+ for (let key in cities) {
+  console.log(`${key}: ${cities[key][0]}, ${cities[key][1]}`);
+}
+ 
+
